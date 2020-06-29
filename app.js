4bf7d246f3c9656a949f39872fc8de7fec33e2bc
Christine Martino <christine.martino@thinkcompany.com>
2020-06-29 16:19:01 -0400

    Add some means of getting product data from button

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
+const store = require("data.json");
+
+class Product {
+  constructor() {
+    this.selector = ".product";
+    this.$productList;
+    this.$product;
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    const productId = event.currentTarget.dataset.id;
+    // const productDetails = store.filter(item => item.id === productId);
+    // console.warn("store:", store);
+  }
+
+  init() {
+    this.$productList = document.querySelectorAll(this.selector);
+
+    this.$productList.forEach(element => {
+      element.addEventListener("click", this.handleClick);
+    });
+  }
+}
+
 class App {
   init() {
     // Initialize on Load.
-    window.onload = event => {
-      console.log(event);
+    window.onload = () => {
+      // Initialize all the things
+      const product = new Product();
+      product.init();
     };
   }
 }
