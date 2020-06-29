267cd892ad4e9619bf7ec9022d7924580a173728
Christine Martino <christine.martino@thinkcompany.com>
2020-06-29 17:18:25 -0400

    Import data and render products in store

--- src/scripts/product.js
+++ src/scripts/product.js
@@ -3,25 +3,54 @@ import store from "../data/data.json";
 class Product {
   constructor() {
     this.selector = ".product";
-    this.$productList;
+    this.$productNodes;
+    this.$store;
     this.$product;
+    this.selectedProduct;
 
     this.handleClick = this.handleClick.bind(this);
+    this.loadProducts = this.loadProducts.bind(this);
+  }
+
+  setupEventHandler() {
+    this.$productNodes = document.querySelectorAll(this.selector);
+
+    this.$productNodes.forEach(element => {
+      element.addEventListener("click", this.handleClick);
+    });
+  }
+
+  renderProducts() {
+    let inventory = "";
+
+    store.forEach(item => {
+      inventory += `<div class="product product-${item.id}" data-id="${item.id}"><button>${item.product} ${item.cost}</button></div>`;
+    });
+
+    const blob = document.createRange().createContextualFragment(inventory);
+    return blob;
+  }
+
+  loadProducts() {
+    const products = this.renderProducts();
+
+    this.$inventory.appendChild(products);
   }
 
   handleClick(event) {
     const productId = event.currentTarget.dataset.id;
-    const productDetails = store.filter(item => item.id === productId);
-    console.warn("store:", store);
-    console.warn("productDetails:", productDetails);
+    this.selectedProduct = store.filter(
+      item => item.id === Number(productId)
+    )[0];
+
+    console.warn("this.selectedProduct:", this.selectedProduct);
   }
 
   init() {
-    this.$productList = document.querySelectorAll(this.selector);
+    this.$inventory = document.querySelector("[data-inventory]");
 
-    this.$productList.forEach(element => {
-      element.addEventListener("click", this.handleClick);
-    });
+    this.loadProducts();
+    this.setupEventHandler();
   }
 }
 
