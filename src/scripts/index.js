238cc2a94ee0aa7d0ddc9a07d903cca581ebc967
Christine Martino <christine.martino@thinkcompany.com>
2020-07-15 12:48:54 -0400

    Refactor index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,5 @@
 import "../styles/index.scss";
-import Product from "./product.js";
-
-console.log("webpack starterkit");
-
-class App {
-  init() {
-    // Initialize on Load.
-    window.onload = () => {
-      // Initialize all the things
-      const product = new Product();
-      product.init();
-    };
-  }
-}
+import App from "./app.js";
 
 // Run the App
 const app = new App();
