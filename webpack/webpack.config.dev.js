4ce454420b1e369d33fe1fec85df88cce4bc1f1e
Christine Martino <christine.martino@thinkcompany.com>
2020-07-28 13:17:56 -0400

    Update styles slightly

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,13 +1,13 @@
-const Path = require('path');
-const Webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const Path = require("path");
+const Webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'cheap-eval-source-map',
+  mode: "development",
+  devtool: "cheap-eval-source-map",
   output: {
-    chunkFilename: 'js/[name].chunk.js'
+    chunkFilename: "js/[name].chunk.js"
   },
   devServer: {
     inline: true,
@@ -15,28 +15,28 @@ module.exports = merge(common, {
   },
   plugins: [
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      "process.env.NODE_ENV": JSON.stringify("development")
     })
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: Path.resolve(__dirname, '../src'),
-        enforce: 'pre',
-        loader: 'eslint-loader',
+        include: Path.resolve(__dirname, "../src"),
+        enforce: "pre",
+        loader: "eslint-loader",
         options: {
           emitWarning: true
         }
       },
       {
         test: /\.js$/,
-        include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader'
+        include: Path.resolve(__dirname, "../src"),
+        loader: "babel-loader"
       },
       {
         test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   }
